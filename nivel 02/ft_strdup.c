6c267f630e6ad050b686a96c67054e10f0a0c6d0
Dalila <dalila@MacBook-Pro-de-Dalila.local>
2024-03-19 20:35:34 +0100

    ft_strdup completed

---
+++ nivel 02/ft_strdup.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <libc.h>
+
+static int  ft_strlen(char *src)
+{
+    int i = 0;
+    while (src[i])
+        i++;
+    return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+    int len;
+    int i;
+    char    *duplicated;
+
+    if (!src)
+        return (NULL);
+    len = ft_strlen(src);
+    i = 0;
+    duplicated = (char *)malloc((len + 1) * sizeof(char));
+    if (!duplicated)
+        return (NULL);
+    while (i < len)
+    {
+        duplicated[i] = src[i];
+        i++;
+    }
+    duplicated[i] = '\0';
+    return (duplicated);
+}
+/*
+int main(void)
+{
+    char    *src = "hi hi hello";
+    char    *duplicated;
+
+    duplicated = ft_strdup(src);
+    printf("%s\n", duplicated);
+    free (duplicated);
+    return (0);
+}*/
